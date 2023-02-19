baf535ba11bc113e795ffce41114911fb383037e
jMoscato <110347584+moscato@users.noreply.github.com>
2023-02-19 12:51:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,26 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server listening at ${PORT}`));
 
-app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
 
 const database = new Datastore("database.db");
 database.loadDatabase();
 
-app.get('/api', (request, response) => {
-    database.find({}, (err, data) => {
-        if (err) {
-            response.end();
-            return;
-        }
-        response.json(data);
-    });
+app.get("/api", (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.send(data);
+  });
 });
 
-app.post('/api', (request, response) => {
-    console.log('I got a request!');
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
-    database.insert(data);
-    response.json(data);
+app.post("/api", (request, response) => {
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
 });
