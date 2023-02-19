9305a9c13c9f960b553b80af08a5551cdb378382
jMoscato <110347584+moscato@users.noreply.github.com>
2023-02-19 12:11:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server listening at ${PORT}`));
+
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
