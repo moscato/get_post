73b6209686f24fec588c8ea490e9b1bbb7114980
jMoscato <110347584+moscato@users.noreply.github.com>
2023-02-19 17:11:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -22,11 +22,25 @@ app.get('/', (req, res) => {
             <br><hr><br>
             <form method="post" action="/data">
               <input type="text" name="text" />
-              <button type="submit">Add</button>
+              <button type="submit" class="sendBtn">Submit!</button>
             </form>
             <ul>
               ${data.map(d => `<li>${d.text}</li>`).join('')}
             </ul>
+          <script>
+            // Define a function to fetch the latest data from the server
+            function fetchLatestData() {
+              fetch('/data')
+                .then(response => response.text())
+                .then(data => {
+                  // Update the HTML with the latest data
+                  document.querySelector('ul').innerHTML = data;
+                });
+            }
+
+            // Call the fetchLatestData function every 5 seconds
+            setInterval(fetchLatestData, 5000);
+          </script>
           </body>
         </html>
       `);
@@ -46,6 +60,21 @@ app.post('/data', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000
+app.get('/data', (req, res) => {
+  db.find({}, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      res.send(`
+        <ul>
+          ${data.map(d => `<li>${d.text}</li>`).join('')}
+        </ul>
+      `);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server listening at ${PORT}`));
